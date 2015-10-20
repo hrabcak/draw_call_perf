c51b28b7cdc7c8f5d16b8c89465a0921102c8307
Ladislav Hrabcak <hrabcak@outerra.com>
2015-10-20 16:06:19 +0200

    backup

--- bin/run_tests.bat
+++ bin/run_tests.bat
@@ -1,5 +1,4 @@
-dctest64.exe --test0 --tex-mode0 --mesh-size0 --tex-freq0
-dctest64.exe --test0 --tex-mode0 --mesh-size0 --tex-freq0
-dctest64.exe --test0 --tex-mode0 --mesh-size0 --tex-freq0
-dctest64.exe --test0 --tex-mode0 --mesh-size0 --tex-freq0
-dctest64.exe --test0 --tex-mode0 --mesh-size0 --tex-freq0
+dctest64.exe --test0 --tex-mode0 --mesh-size2 --tex-freq0
+dctest64.exe --test0 --tex-mode1 --mesh-size2 --tex-freq0
+dctest64.exe --test0 --tex-mode2 --mesh-size2 --tex-freq0
+dctest64.exe --test0 --tex-mode3 --mesh-size2 --tex-freq0
