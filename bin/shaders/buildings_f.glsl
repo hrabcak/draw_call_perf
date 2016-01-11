f6bd2ac2118e9f26f2e6d1f31ac9c8628476f421
Cyril <cyril.gramblicka@gmail.com>
2016-01-11 15:18:03 +0100

    shaders comment/purge

--- bin/shaders/buildings_f.glsl
+++ bin/shaders/buildings_f.glsl
@@ -11,8 +11,5 @@ void main()
 {
 	vec3 norm = normalize(cross(dFdx(inp.wpos), dFdy(inp.wpos)));
 	float d = clamp(dot(norm, normalize(vec3(1, 1, 0))), 0, 1);
-	//_retval = vec4(vec3(d * 0.7 + 0.3), 0);
-	//_retval = vec4(norm * 0.5 + 0.5, 0);
-	//_retval = vec4(0,0,0,0);
 	_retval = vec4(norm,0.0);
 }
--- bin/shaders/buildings_v_n.glsl
+++ bin/shaders/buildings_v_n.glsl
@@ -10,36 +10,22 @@ out OUT{
 	vec3 wpos;
 } o;
 
-struct Building{
-	uint flags;
-	vec2 pnts[4];
-};
+/*
+	Buiding packing
+	base_point - relative to tile
+			   - 16bits per component
+	ohter 3 points - relative to base point
+				   - 13 bits per components
 
-Building unpack_ivec4_to_building(ivec4 packed_block){
-	Building result;
-	const float scale16 = 2500.0f / 32767.0f;
-	const float scale13 = 312.5f / 4095.0f;
+	ivec4{
+			upper bits -> lower bits
+		x - base_point.x(16 bits), base_point.y(16 bits)
+		y - p1.x(13 bits), p1.y(13 bits),p2.x(6 bits)
+		z - p2.x(7 bits), p2.y(13 bits), p3.x(12 bits)
+		w - p3.x(1 bit),p3.y(13 bits), falgs (18 bits)
+	}
 
-	result.pnts[0].x = (packed_block.x >> 16) * scale16;
-	result.pnts[0].y = ((packed_block.x << 16) >> 16) * scale16;
-
-	result.pnts[1].x = (packed_block.y >> 19) * scale13;
-	result.pnts[1].y = ((packed_block.y << 13) >> 19) * scale13;
-
-	result.pnts[2].x = (((packed_block.y << 26) >> 19) | ((packed_block.z >> 25) & 0x7f)) * scale13;
-	result.pnts[2].y = ((packed_block.z << 7) >> 19) * scale13;
-
-	result.pnts[3].x = (((packed_block.z << 20) >> 19) | ((packed_block.w >> 31) & 0x1))* scale13;
-	result.pnts[3].y = ((packed_block.w << 1) >> 19)* scale13;
-
-	result.pnts[1] += result.pnts[0];
-	result.pnts[2] += result.pnts[0];
-	result.pnts[3] += result.pnts[0];
-
-	result.flags = uint(packed_block.w & 0xff);
-
-	return result;
-}
+*/
 
 vec2 unpack_ivec4_to_p1(ivec4 packed_block){
 	vec2 result;
@@ -92,30 +78,32 @@ void main()
 	int vtxID = gl_VertexID & 7;
 	int index = gl_InstanceID * BLOCKS_PER_IDC + (gl_VertexID >> 3);
 
-	 /*(index >= total_count){
-		gl_Position = vec4(0.0, 0.0, 0.0, 0.0);
+	 if(index >= total_count){
+		gl_Position = vec4(0.0, 0.0, 0.0, 0.0); 
 		return;
-	}*/
+	}
 
 	ivec4 block = texelFetch(tb_blocks, index);
 	
 	vec2 xy;
 	uint flags = unpack_ivec4_to_flags(block);
 
+	vec2 base_point = unpack_ivec4_to_p1(block) + vec2(tile_offset) * t_width; // base point of buidling
+
 	if ((vtxID & 3) == 0){
-		xy = unpack_ivec4_to_p1(block) + vec2(tile_offset) * t_width;
+		xy = base_point;
 	}
 	else if((vtxID & 3) == 1){
-		xy = unpack_ivec4_to_p1(block) + unpack_ivec4_to_p2(block) + vec2(tile_offset) * t_width;
+		xy = base_point + unpack_ivec4_to_p2(block);
 	}
 	else if((vtxID & 3) == 2){
-		xy = unpack_ivec4_to_p1(block) + unpack_ivec4_to_p3(block) + vec2(tile_offset) * t_width;
+		xy = base_point + unpack_ivec4_to_p3(block);
 	}
 	else if((vtxID & 3) == 3){
-		xy = unpack_ivec4_to_p1(block) + unpack_ivec4_to_p4(block) + vec2(tile_offset) * t_width;
+		xy = base_point + unpack_ivec4_to_p4(block);
 	}
 
-	o.wpos = vec3(xy.x + float(total_count) * 0.0000000000001, (vtxID > 3) ? (float(flags) * 2.9) * 0.5 : 0.0, -xy.y);
+	o.wpos = vec3(xy.x, (vtxID > 3) ? (float(flags) * 2.9) * 0.5 : 0.0, -xy.y);
 	vec4 opos = mvp * vec4(o.wpos, 1);
 
 	opos.z = log2(max(1e-6, 1.0 + opos.w)) / log(1000000 + 1.0) - 1.0;
