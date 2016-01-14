3790825d5d06e6d68a741c88345ba7a2a71102c8
Cyril <cyril.gramblicka@gmail.com>
2016-01-14 15:54:39 +0100

    cube test update

--- bin/cubes_test.bat
+++ bin/cubes_test.bat
@@ -27,4 +27,16 @@ dctest64.exe --test2 --mesh-size 11 --tex-size32 --var_cube_size
 dctest64.exe --test2 --mesh-size 12 --tex-size32 --var_cube_size 
 dctest64.exe --test2 --mesh-size 13 --tex-size32 --var_cube_size 
 
+dctest64.exe --test2 --mesh-size 2 --tex-size32
+dctest64.exe --test2 --mesh-size 3 --tex-size32
+dctest64.exe --test2 --mesh-size 4 --tex-size32
+dctest64.exe --test2 --mesh-size 5 --tex-size32
+dctest64.exe --test2 --mesh-size 6 --tex-size32
+dctest64.exe --test2 --mesh-size 7 --tex-size32
+dctest64.exe --test2 --mesh-size 8 --tex-size32
+dctest64.exe --test2 --mesh-size 9 --tex-size32
+dctest64.exe --test2 --mesh-size 10 --tex-size32
+dctest64.exe --test2 --mesh-size 11 --tex-size32
+dctest64.exe --test2 --mesh-size 12 --tex-size32
+dctest64.exe --test2 --mesh-size 13 --tex-size32
  
