fb5f0aea50d4e734dbea27afe257a8598ab5f4c5
Cyril <cyril.gramblicka@gmail.com>
2016-01-14 15:41:45 +0100

    new cubes bat VBO

--- bin/cubes_test.bat
+++ bin/cubes_test.bat
@@ -1,19 +1,18 @@
 @echo off
 del /Q cubes_test.csv
 
-dctest64.exe --test2 --mesh-size 2 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 3 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 4 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 5 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 6 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 7 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 8 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 9 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 10 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 11 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 12 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 13 --tex-size32 --var_cube_size --nocull
-dctest64.exe --test2 --mesh-size 14 --tex-size32 --var_cube_size --nocull
+dctest64.exe --test2 --mesh-size 2 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 3 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 4 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 5 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 6 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 7 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 8 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 9 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 10 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 11 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 12 --tex-size32 --var_cube_size --use-vbo
+dctest64.exe --test2 --mesh-size 13 --tex-size32 --var_cube_size --use-vbo
 
 dctest64.exe --test2 --mesh-size 2 --tex-size32 --var_cube_size 
 dctest64.exe --test2 --mesh-size 3 --tex-size32 --var_cube_size 
@@ -27,6 +26,5 @@ dctest64.exe --test2 --mesh-size 10 --tex-size32 --var_cube_size
 dctest64.exe --test2 --mesh-size 11 --tex-size32 --var_cube_size 
 dctest64.exe --test2 --mesh-size 12 --tex-size32 --var_cube_size 
 dctest64.exe --test2 --mesh-size 13 --tex-size32 --var_cube_size 
-dctest64.exe --test2 --mesh-size 14 --tex-size32 --var_cube_size 
 
  
