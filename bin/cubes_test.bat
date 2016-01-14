7ea56b846a098dd14e0e3eace3a7ded56f93d061
Cyril <cyril.gramblicka@gmail.com>
2016-01-14 10:29:28 +0100

    cube test bat

--- bin/cubes_test.bat
+++ bin/cubes_test.bat
@@ -1,15 +1,34 @@
 @echo off
 del /Q cubes_test.csv
 
-dctest64.exe --test2 --mesh-size 1
-dctest64.exe --test2 --mesh-size 2
-dctest64.exe --test2 --mesh-size 3
-dctest64.exe --test2 --mesh-size 4
-dctest64.exe --test2 --mesh-size 5
-dctest64.exe --test2 --mesh-size 6
-dctest64.exe --test2 --mesh-size 7
-dctest64.exe --test2 --mesh-size 8
-dctest64.exe --test2 --mesh-size 9
-dctest64.exe --test2 --mesh-size 10
-dctest64.exe --test2 --mesh-size 11
-dctest64.exe --test2 --mesh-size 12
+dctest64.exe --test2 --mesh-size 1 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 2 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 3 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 4 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 5 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 6 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 7 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 8 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 9 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 10 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 11 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 12 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 13 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 14 --tex-size32 --nocull
+dctest64.exe --test2 --mesh-size 15 --tex-size32 --nocull
+
+dctest64.exe --test2 --mesh-size 1 --tex-size32
+dctest64.exe --test2 --mesh-size 2 --tex-size32
+dctest64.exe --test2 --mesh-size 3 --tex-size32
+dctest64.exe --test2 --mesh-size 4 --tex-size32
+dctest64.exe --test2 --mesh-size 5 --tex-size32
+dctest64.exe --test2 --mesh-size 6 --tex-size32
+dctest64.exe --test2 --mesh-size 7 --tex-size32
+dctest64.exe --test2 --mesh-size 8 --tex-size32
+dctest64.exe --test2 --mesh-size 9 --tex-size32
+dctest64.exe --test2 --mesh-size 10 --tex-size32
+dctest64.exe --test2 --mesh-size 11 --tex-size32
+dctest64.exe --test2 --mesh-size 12 --tex-size32
+dctest64.exe --test2 --mesh-size 13 --tex-size32
+dctest64.exe --test2 --mesh-size 14 --tex-size32
+dctest64.exe --test2 --mesh-size 15 --tex-size32
